11b1b1dd7990d60c138710c4b8493c099c1b3825
okimesha <okiimesha@gmail.com>
2026-06-25 21:18:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import traceback
 import joblib
 
 try:
-    model = joblib.load("best_model.pkl")
-    scaler = joblib.load("scaler.pkl")
+    model = joblib.load("best_model (1).pkl")
+    scaler = joblib.load("scaler (1).pkl")
 except Exception:
     st.code(traceback.format_exc())
     st.stop()
